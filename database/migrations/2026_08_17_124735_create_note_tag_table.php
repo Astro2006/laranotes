261ecc0c39fd4a67bd8e98b312ce778274b05e37
Julian Leipert <juli.leipert@gmail.com>
2026-08-17 15:27:16 +0200

    wip

--- database/migrations/2026_08_17_124735_create_note_tag_table.php
+++ database/migrations/2026_08_17_124735_create_note_tag_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Notes;
+use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('note_tag', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('note_id')->constrained('notes')->cascadeOnDelete();
-            $table->foreignId('tag_id')->constrained('tags')->cascadeOnDelete();
+            $table->foreignIdFor(Notes::class, 'note_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Tag::class, 'tag_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
 
             $table->unique(['note_id', 'tag_id']);
