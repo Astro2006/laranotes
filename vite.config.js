0fed3230bf33578faf841c0c3381fd1f1b47e73b
Julian Leipert <juli.leipert@gmail.com>
2026-08-18 13:35:28 +0200

    make app work

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,30 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import laravel from "laravel-vite-plugin";
+import { bunny } from "laravel-vite-plugin/fonts";
+import { defineConfig } from "vite";
+import {
+    nativephpHotFile,
+    nativephpMobile,
+} from "./vendor/nativephp/mobile/resources/js/vite-plugin.js";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
+            hotFile: nativephpHotFile(),
             fonts: [
-                bunny('Instrument Sans', {
+                bunny("Instrument Sans", {
                     weights: [400, 500, 600],
                 }),
             ],
         }),
         tailwindcss(),
+        nativephpMobile(),
     ],
     server: {
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
